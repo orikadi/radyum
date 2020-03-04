ac1663931a73790ca3386a655d9028b6a8860b16
yoav <yoavle3@gmail.com>
2020-03-04 18:05:49 +0200

    added picture editing button and saving to firebase

--- radyum/ProfilePgeViewController.swift
+++ radyum/ProfilePgeViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 import Firebase
 import Kingfisher
 
-class ProfilePgeViewController: UIViewController {
+class ProfilePgeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     var user:User?
     //TODO: add spinner to review table in user profile
     @IBOutlet weak var userNameTitle: UILabel!
 //    @IBOutlet weak var numOfReviewsTitle: UILabel!
     @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var editPictureButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,8 +36,31 @@ class ProfilePgeViewController: UIViewController {
         }
     }
     
+    @IBAction func editPicture(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            imagePicker.sourceType =
+                UIImagePickerController.SourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
     
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let newPic = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        if(newPic != nil && newPic != self.picture.image){
+            self.picture.image = newPic
+            Model.modelFirebaseInstance.saveImage(image: self.picture.image!) { (url) in
+                self.user?.avatar = url
+                Model.modelFirebaseInstance.editUserPicUrl(url: url)
+            }
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
